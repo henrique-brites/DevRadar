320f0cfdf4b5a9e1ab9a4652e97aea827e1f99f1
Henrique Brites <henriquebrites@live.com>
2020-01-23 14:02:14 -0300

    backend

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,111 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(request, response) {
+        try {
+            const devs = await Dev.find();
+
+            return response.json(devs); 
+        } catch (err) {
+            return response.status(400).send({error: 'Error loading devs'})
+        }
+    },
+
+    async show(request, response) {
+
+        try {
+            const dev = await Dev.findOne({ _id: request.params.devId});
+            if (dev) {
+                return response.json(dev);
+            }
+        } catch (error) {
+            return response.status(400).send({ error: 'Error loading dev' });
+        }
+        
+        return response.status(400).send();
+
+    },
+
+    async store(request, response) {
+        try {
+            const { github_username, techs, latitude, longitude } = request.body;
+
+            let dev = await Dev.findOne({ github_username });
+
+            if (!dev) {
+                const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+        
+                const { name = login, avatar_url, bio } = apiResponse.data;
+            
+                const techsArray = parseStringAsArray(techs);
+            
+                const location = {
+                    type: 'Point',
+                    coordinates: [longitude, latitude],
+                }                         
+            
+                dev = await Dev.create({
+                    github_username,
+                    name,
+                    avatar_url,
+                    bio,
+                    techs: techsArray,
+                    location,
+                });
+            };
+            return response.json(dev);
+        } catch (err) {
+            return response.status(400).send({ error: 'Error creating new dev' });
+        }
+    },    
+
+    async update(request, response) {
+        //console.log(request.body);
+        try {
+            const { techs, latitude, longitude } = request.body; //github_username, 
+             
+             let dev = await Dev.findOne({ _id: request.params.devId});
+
+            if (dev) {
+
+                const apiResponse = await axios.get(`https://api.github.com/users/${dev.github_username}`);
+        
+                const { name = login, avatar_url, bio } = apiResponse.data;
+            
+                const techsArray = parseStringAsArray(techs);
+            
+                const location = {
+                    type: 'Point',
+                    coordinates: [longitude, latitude],
+                }                         
+
+                dev = await Dev.findByIdAndUpdate(request.params.devId, { //  findOneAndUpdate
+                    name,
+                    avatar_url,
+                    bio,
+                    techs: techsArray,
+                    location,
+                }, { new: true });
+
+            };
+            return response.json(dev);        
+        } catch (err) {
+            return response.status(400).send({ error: 'Error updating dev' });
+        }        
+      },
+
+    async delete(request, response) {
+        //return response.status(400).send(request.params.devId);
+        try {
+            //await Dev.findOneAndDelete(request.params.devId);
+            await Dev.findByIdAndDelete(request.params.devId); 
+        
+            //return response.send('Dev Deletado!!');
+            return response.send();
+        } catch (err) {
+            return response.status(400).send({ error: 'Error deleting dev' });
+        }
+    },
+};
---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,28 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index( request, response) {
+        const { latitude, longitude, techs} = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        const devs = await Dev.find({
+           techs: {
+                $in: techsArray,
+           },
+           location: {
+               $near: {
+                   $geometry: {
+                       type: 'Point',
+                       coordinates: [longitude, latitude],
+                   },
+                   $maxDistance: 1000,
+               },
+           },
+        });
+        // Busca todos os devs bum raio 10 km
+        // Filtrar por tecnologias
+        return response.json({ devs });
+    }
+}
